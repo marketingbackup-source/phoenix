2f743888440bf1ebb703ea22eb493e19ec0a38db
marketingbackup-source <marketingbackup@pcba.com.au>
2026-09-23 10:42:55 +0530

    cache cleared

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -40,7 +40,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     },
 
     {
-      url: `${BASE_URL}/about`,
+      url: `${BASE_URL}/about-us`,
       changeFrequency: "monthly",
       priority: 0.8,
     },
